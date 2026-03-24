4fd36634686b30a8439e11f4b573ccf61b83af8c
wangrushen <wangrushen369@gmail.com>
2026-03-24 14:21:42 +0800

    fix: mysqld_exporter socket config

--- mysqloperator/controller/innodbcluster/cluster_api.py
+++ mysqloperator/controller/innodbcluster/cluster_api.py
@@ -448,7 +448,7 @@ class MetriscSpec:
 
             config = f"""[client]
 user={self.dbuser_name}
-socket=unix:///var/run/mysqld/mysql.sock
+socket=/var/run/mysqld/mysql.sock
 """
 
             return [
